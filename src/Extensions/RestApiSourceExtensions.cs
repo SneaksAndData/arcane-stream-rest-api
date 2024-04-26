efb6a8bc1122d93734a02b940ef18819bccbe647
Vitalii Savitskii <visa@ecco.com>
2024-04-26 15:41:57 +0200

    Implement the RestAPI arcane stream (#13)
    * Implement the RestAPI arcane stream
    
    * Add stream context runtime loading

---
+++ src/Extensions/RestApiSourceExtensions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Akka.Streams;
+using Akka.Streams.Dsl;
+using Arcane.Framework.Contracts;
+using Arcane.Framework.Services.Base;
+using Arcane.Framework.Sinks.Json;
+using Arcane.Framework.Sources.RestApi;
+using Parquet.Data;
+using Snd.Sdk.Metrics.Base;
+using Snd.Sdk.Storage.Base;
+
+namespace Arcane.Stream.RestApi.Extensions;
+
+public static class RestApiSourceExtensions
+{
+    
+    public static IRunnableGraph<(UniqueKillSwitch, Task)> BuildGraph(this RestApiSource source,
+        MetricsService metricsService,
+        IBlobStorageWriter blobStorageWriter,
+        string sinkLocation,
+        IStreamContext context,
+        int rowsPerGroup,
+        TimeSpan groupingInterval)
+    {
+        var dimensions = source.GetDefaultTags().GetAsDictionary();
+        var jsonSink = context.MultilineJsonSinkFromContext(source.GetParquetSchema(), blobStorageWriter, sinkLocation);
+        return Source.FromGraph(source)
+            .GroupedWithin(rowsPerGroup, groupingInterval)
+            .Select(grp =>
+            {
+                var rows = grp.ToList();
+                metricsService.Increment(DeclaredMetrics.ROWS_INCOMING, dimensions, rows.Count);
+                return rows;
+            })
+            .Log(context.StreamKind)
+            .ViaMaterialized(KillSwitches.Single<List<JsonElement>>(), Keep.Right)
+            .ToMaterialized(jsonSink, Keep.Both);
+    }
+    
+
+    public static MultilineJsonSink MultilineJsonSinkFromContext(this IStreamContext streamContext, Schema schema, IBlobStorageWriter blobStorageWriter, string sinkLocation)
+    {
+        var jsonSink = MultilineJsonSink.Create(
+            blobStorageWriter,
+            $"{sinkLocation}/{streamContext.StreamId}",
+            schema,
+            streamContext.IsBackfilling ? "backfill" : "data",
+            dropCompletionToken: streamContext.IsBackfilling);
+        return jsonSink;
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Reflection;
+using Arcane.Framework.Contracts;
+using Arcane.Framework.Providers;
+using Arcane.Framework.Providers.Hosting;
+using Arcane.Stream.RestApi.Models;
+using Arcane.Stream.RestApi.Models.Base;
+using Arcane.Stream.RestApi.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Snd.Sdk.Logs.Providers;
+using Snd.Sdk.Metrics.Providers;
+using Snd.Sdk.Storage.Base;
+using Snd.Sdk.Storage.Providers;
+using Snd.Sdk.Storage.Providers.Configurations;
 
-using System;
+Log.Logger = DefaultLoggingProvider.CreateBootstrapLogger(nameof(Arcane));
+int exitCode;
+try
+{
+    exitCode = await Host.CreateDefaultBuilder(args)
+        .AddDatadogLogging()
+        .ConfigureRequiredServices(services =>
+        {
+            return services.AddStreamGraphBuilder<RestApiGraphBuilder>(context =>
+            {
+                // Dynamically load the stream context based on the stream kind
+                // This logic is used to determine the stream context type based on the stream kind
+                // Until the StreamComponent is implemented, this .
+                var contextNS = typeof(RestApiFixedAuthStreamContext).Namespace;
+                var typeFullName = $"{contextNS}.{context.StreamKind}StreamContext";
+                var targetType = Assembly.GetExecutingAssembly().GetType(typeFullName);
+                return ((RestApiStreamContextBase)StreamContext.ProvideFromEnvironment(targetType)).LoadSecrets();
+            });
+        })
+        .ConfigureAdditionalServices((services, context) =>
+         {
+             services.AddAzureBlob(AzureStorageConfiguration.CreateDefault());
+             services.AddDatadogMetrics(context.ApplicationName);
+             services.AddSingleton<IBlobStorageWriter>(sp => sp.GetRequiredService<IBlobStorageService>());
+         })
+    .Build()
+    .RunStream(Log.Logger);
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Host terminated unexpectedly");
+    return ExitCodes.FATAL;
+}
+finally
+{
+    await Log.CloseAndFlushAsync();
+}
 
-Console.WriteLine("Hello, World!");
+return exitCode;
+
