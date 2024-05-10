5cb6637e7588279ef5c7b122b59823841b73fa10
Vitalii Savitskii <visa@ecco.com>
2024-05-10 11:52:17 +0200

    Fix service signal registration

--- src/Hosting.cs
+++ src/Hosting.cs
@@ -18,39 +18,41 @@ public static class Hosting
         var runner = host.Services.GetRequiredService<IStreamRunnerService>();
         var context = host.Services.GetRequiredService<IStreamContext>();
         // using var lifetimeService = host.Services.GetRequiredService<IStreamLifetimeService>();
-        using var reg = SetupSignalHandler(runner);
-        var graphBuilder = host.Services.GetRequiredService<IStreamGraphBuilder<IStreamContext>>();
-        try
+        using (var reg = SetupSignalHandler(runner))
         {
-            var completeTask = runner.RunStream(() => graphBuilder.BuildGraph(context));
-            await completeTask;
-        }
-        catch (Exception e)
-        {
-                if(handleUnknownException is null)
-                {
-                    return FatalExit(e, logger);
-                }
-                return await handleUnknownException(e) switch
-                {
-                    {HasValue: true, Value: var exitCode} => exitCode,
-                    _ => FatalExit(e, logger),
-                };
-        }
+            var graphBuilder = host.Services.GetRequiredService<IStreamGraphBuilder<IStreamContext>>();
+            try
+            {
+                var completeTask = runner.RunStream(() => graphBuilder.BuildGraph(context));
+                await completeTask;
+            }
+            catch (Exception e)
+            {
+                    if(handleUnknownException is null)
+                    {
+                        return FatalExit(e, logger);
+                    }
+                    return await handleUnknownException(e) switch
+                    {
+                        {HasValue: true, Value: var exitCode} => exitCode,
+                        _ => FatalExit(e, logger),
+                    };
+            }
 
-        logger.Information("Streaming job is completed successfully, exiting");
-        return ExitCodes.SUCCESS;
+            logger.Information("Streaming job is completed successfully, exiting");
+            return ExitCodes.SUCCESS;
+        }
     }
     
     private static PosixSignalRegistration SetupSignalHandler(IStreamRunnerService streamRunnerService,
         PosixSignal signal = PosixSignal.SIGTERM)
     {
+        Log.Information( "Registered signal handler for SIGTERM");
         return PosixSignalRegistration.Create(PosixSignal.SIGTERM, context => 
         {
             context.Cancel = true;
             Log.Information("Received a signal {signal}. Stopping the hosted stream and shutting down application", signal);
-            // streamRunnerService.StopStream();
-            throw new Exception("SIGNAL");
+            streamRunnerService.StopStream();
         });
     }
 
