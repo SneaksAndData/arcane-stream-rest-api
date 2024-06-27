fdbbc1518ae748f37dd1a6e36e4d5e59205a92f2
Vitalii Savitskii <visa@ecco.com>
2024-06-27 16:28:58 +0200

    Add ability to use legacy abfss paths

---
+++ src/Extensions/IStreamContextExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Arcane.Framework.Services.Base;
+using Arcane.Stream.RestApi.Models;
+
+namespace Arcane.Stream.RestApi.Extensions;
+
+public static class IStreamContextExtensions
+{
+    public static string GetSinkLocation(this IStreamContext streamContext)
+    {
+        return streamContext switch
+        {
+            RestApiDynamicAuthStreamContext restApiPagedFixedAuthStreamContext => restApiPagedFixedAuthStreamContext.SinkLocation,
+            RestApiFixedAuthStreamContext restApiFixedAuthStreamContext => restApiFixedAuthStreamContext.SinkLocation,
+            RestApiPagedDynamicAuthStreamContext restApiPagedDynamicAuthStreamContext => restApiPagedDynamicAuthStreamContext.SinkLocation,
+            RestApiPagedFixedAuthStreamContext restApiPagedFixedAuthStreamContext => restApiPagedFixedAuthStreamContext.SinkLocation,
+            _ => throw new InvalidOperationException($"Unknown stream context type {streamContext.GetType()}")
+        };
+    }
+}
